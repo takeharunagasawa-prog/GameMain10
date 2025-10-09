b964a45bc2fe36d4d8daca2ffa13a58a8bb6c809
nishida249102786-dot <nishida249102786@gmail.com>
2025-10-09 16:27:39 +0900

    タイトルシーンからゲームシーンへ

---
+++ Assets/nishida-777/Script/GameTitleScene.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameTitleScene : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SceneManager.LoadScene("GameTitle");
+    }
+    //public void 
+}
