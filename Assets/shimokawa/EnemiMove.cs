22135911edb730205b3449a3566b56ac24cce023
mana2974 <shimokawa229088407@gmail.com>
2025-10-09 15:53:42 +0900

    エネミー　アニメーション

--- Assets/shimokawa/EnemiMove.cs
+++ Assets/shimokawa/EnemiMove.cs
@@ -3,18 +3,25 @@ using UnityEngine;
 public class EnemiMove : MonoBehaviour
 {
     [SerializeField] private float speed = 2f;          //敵の移動速度
+    [SerializeField] private float vanishTime = 1.0f;
+    [SerializeField] private float explossionVanishTime = 1.0f;
     [SerializeField] private Transform core;         //核の位置情報
+    [SerializeField] private GameObject explossion;
+    [SerializeField] private GameObject arrowHit;
     private Animator animator;
+    private bool isActive = true;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        animator = GetComponent<Animator>();
+        animator = GetComponent<Animator>();       
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!isActive) { return; }
+
         if (core == null) return;//核が見つからない場合は何もしない
 
         //プレイヤーの方向を求める
@@ -28,9 +35,25 @@ public class EnemiMove : MonoBehaviour
     {
         core = target;
     }
-    
+    private void Explossion()
+    {
+        Instantiate(explossion, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
     public void Defeated(bool isExplode)
     {
+        if (!isActive) { return; }
+        isActive = false;
+
         animator.SetTrigger("Defeated");
+        if (isExplode) 
+        {
+            Invoke("Explossion", explossionVanishTime);
+        }
+        else
+        {
+            Instantiate(arrowHit, transform.position, Quaternion.identity);
+            Destroy(gameObject, vanishTime);
+        }
     }
 }
